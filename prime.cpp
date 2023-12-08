e0038b744f8ba181aabb684675eaf3d18b5004ab
Lei Zhang <lei99.zhang@tum.de>
2023-12-08 23:48:03 +0100

    update prime and sum_fibonacci

--- prime.cpp
+++ prime.cpp
@@ -1,23 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int sum_fibonaci (int n) {
-	if (n ==1) {
-		return 1;
-	}
-	int num[n];
-	num[0] = 1;
-	num[1] = 1;
-	cout<<num[0]<<", "<<num[1]<<", ";
-	int sum = 2;
-	for (int i = 2; i < n; i++) {
-		num[i] = num[i-1] + num[i-2];
-		cout<<num[i]<<", ";
-		sum += num[i];
-	}
-	return sum;
-}
 int main() {
-	int n = 7;
-	cout<<endl<<sum_fibonaci(n);
+	bool isprime = 1;
+	int cnt = 0;
+	for (int i = 0; i <= 100; i++) {
+		isprime = 1;
+		for (int j = 2; j < i; j++) {
+			if ((i%j)==0) {
+				isprime = 0;
+				break;
+			}
+		}
+		if (isprime) {
+			cout<<i<<", ";
+			cnt++;
+		}
+	}
+	cout<<"\n"<<cnt;
 }
---
+++ sum_fibonacci.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int sum_fibonacci (int n) {
+	if (n ==1) {
+		return 1;
+	}
+	int num[n];
+	num[0] = 1;
+	num[1] = 1;
+	cout<<num[0]<<", "<<num[1]<<", ";
+	int sum = 2;
+	for (int i = 2; i < n; i++) {
+		num[i] = num[i-1] + num[i-2];
+		cout<<num[i]<<", ";
+		sum += num[i];
+	}
+	return sum;
+}
+int main() {
+	int n = 7;
+	cout<<endl<<sum_fibonacci(n);
+}
